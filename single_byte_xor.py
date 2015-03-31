4bc4ddd4b306580fa9d854cf85a7582fc7317e1b
Michal Witas <bolek117@interia.pl>
2015-03-31 13:39:09 +0200

    Frequency and simple scoring finished

--- single_byte_xor.py
+++ single_byte_xor.py
@@ -1,68 +1,111 @@
 import argparse
-import os
+import string
 
 __author__ = 'mwitas'
+
+# Description: Less number of points is better
 
 
 def simple_score(data):
     result = 0
     for i in range(0, len(data)):
         if data[i] in 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890 ':
-            result += 2
+            pass
+        elif data[i] in string.printable:
+            result += 1
         else:
-            result -= 1
+            result += 2
 
     return result
 
 
-def generate_list():
+def frequency_score(data):
+    lookup = {'A': 0.6433070866141732,
+              'B': 0.1015748031496063,
+              'C': 0.21889763779527557,
+              'D': 0.3346456692913386,
+              'E': 1.0,
+              'F': 0.17559055118110237,
+              'G': 0.15905511811023623,
+              'H': 0.4795275590551181,
+              'I': 0.5488188976377952,
+              'J': 0.011811023622047244,
+              'K': 0.060629921259842526,
+              'L': 0.31732283464566935,
+              'M': 0.18976377952755907,
+              'N': 0.531496062992126,
+              'O': 0.5913385826771653,
+              'P': 0.15196850393700786,
+              'Q': 0.007874015748031498,
+              'R': 0.47165354330708664,
+              'S': 0.4984251968503937,
+              'T': 0.7133858267716536,
+              'U': 0.2173228346456693,
+              'V': 0.07716535433070866,
+              'W': 0.1858267716535433,
+              'X': 0.011811023622047244,
+              'Y': 0.1551181102362205,
+              'Z': 0.005511811023622048
+              }
+
+    data = data.upper()
+
+    frequencies = {}
+    for i in string.ascii_uppercase:
+        frequencies[i] = 0
+
+    score = 0.0
+    max_value = 1
+    count = len(data)
+    for i in xrange(0, len(data)):
+        char = data[i]
+
+        if char in string.ascii_uppercase:
+            count += 1
+            frequencies[char] += 1
+            if frequencies[char] > max_value:
+                max_value = frequencies[char]
+        elif char in string.printable:
+            score += 1
+        else:
+            score += 2
+
+    # Normalize frequencies and generate scores
+    for i in frequencies:
+        frequencies[i] /= max_value
+        score += abs(frequencies[i] - lookup[i])
+
+    return score
 
 
-def frequency_score(sample, data):
-    if not os.path.isfile(sample):
-        raise IOError('Sample must be file')
-    else:
-        handle_sample = open(sample, 'r')
-        text_sample = handle_sample.readall()
+def do_single_byte_xor_brute_force(ciphertext, mode):
+    results = []
+    for i in range(0x00, 0xff):
+        decoded = ''
 
-    if os.path.isfile(data):
-        handle_data = open(data, 'r')
-        text_data = handle_data.readall()
-    else:
-        text_data = data
+        for j in range(0, len(ciphertext)):
+            ch = ciphertext[j]
+            decoded += chr(ord(ch) ^ i)
 
-    src = []
-    dst = []
+        if mode == 'simple':
+            score = simple_score(decoded)
+        else:
+            score = frequency_score(decoded)
 
-    # for i in xrange(0, 1):
-        # generate list
+        result = {'score': score, 'text': decoded}
+        results.append(result)
 
-
-
-
+    return results
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="Ciphertext to crack", type=str)
+    parser.add_argument("-m", "--mode", help="Mode of scoring", default='simple', type=str,
+                        choices=['simple', 'frequency'], dest='mode')
     parser.add_argument("-n", "--results_num", help="Number or best matching results printed to screen (put 0 for all)",
                         default=3, type=int, dest="results_num")
     return parser.parse_args()
-
-
-def do_single_byte_xor_brute_force(ciphertext):
-    results = []
-    for i in range(0x00, 0xff):
-        res = ''
-
-        for j in range(0, len(ciphertext)):
-            ch = ciphertext[j]
-            res += chr(ord(ch) ^ i)
-
-        result_string = "{:04d}: {}".format(simple_score(res), res)
-        results.append(result_string)
-
-    return results
 
 
 def main():
@@ -70,9 +113,12 @@ def main():
 
     data = args.input
     results_num = args.results_num
+    mode = args.mode
 
-    result = do_single_byte_xor_brute_force(data.decode('hex'))
-    result.sort()
+    print(mode)
+
+    result = do_single_byte_xor_brute_force(data.decode('hex'), mode)
+    result.sort(key=lambda res: res['score'] * -1)
 
     if results_num > 0:
         border = len(result) - results_num
@@ -80,7 +126,7 @@ def main():
         border = 0
 
     for i in range(border, len(result)):
-        print(result[i])
+        print('{}: {}'.format(result[i]['score'], result[i]['text']))
 
 
 if __name__ == "__main__":
